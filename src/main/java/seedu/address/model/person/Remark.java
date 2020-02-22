c63e5261655690c00d91ee552804f3b12bb06cc3
Vincent Chow <51158450+thetruevincentchow@users.noreply.github.com>
2020-02-22 14:07:55 +0800

    Implement `remark` command Remarks can now be added with the `remark [index] r/[remark]` command. However, remarks are not saved between sessions.

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -3,13 +3,9 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-/**
- * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
- */
-public class Address {
+public class Remark {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values, and can be empty";
 
     /*
      * The first character of the address must not be a whitespace,
@@ -20,18 +16,18 @@ public class Address {
     public final String value;
 
     /**
-     * Constructs an {@code Address}.
+     * Constructs a {@code Remark}.
      *
-     * @param address A valid address.
+     * @param remark A valid remark.
      */
-    public Address(String address) {
-        requireNonNull(address);
-        checkArgument(isValidAddress(address), MESSAGE_CONSTRAINTS);
-        value = address;
+    public Remark(String remark) {
+        requireNonNull(remark);
+        //checkArgument(isValidAddress(remark), MESSAGE_CONSTRAINTS);
+        value = remark;
     }
 
     /**
-     * Returns true if a given string is a valid address.
+     * Returns true if a given string is a valid remark.
      */
     public static boolean isValidAddress(String test) {
         return test.matches(VALIDATION_REGEX);
@@ -45,13 +41,12 @@ public class Address {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
-                && value.equals(((Address) other).value)); // state check
+                || (other instanceof Remark // instanceof handles nulls
+                && value.equals(((Remark) other).value)); // state check
     }
 
     @Override
     public int hashCode() {
         return value.hashCode();
     }
-
 }
