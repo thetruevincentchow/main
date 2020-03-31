a7b3befd6cd2a384618b2c6266c4ca25ef95626e
Benjamin Lau <capeguy@capeguy.net>
2020-03-31 21:29:22 +0800

    Fixed #88

--- src/main/java/seedu/address/model/module/ModuleDataImporter.java
+++ src/main/java/seedu/address/model/module/ModuleDataImporter.java
@@ -1,5 +1,6 @@
 package seedu.address.model.module;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
@@ -26,28 +27,25 @@ public class ModuleDataImporter {
     private static List<Module> modules = new ArrayList<>();
 
     public static List<Module> run() {
-        String url;
-        Path path;
+        File file;
         Module module;
+        String fileName = "";
         for (String acadYear : acadYears) {
             try {
-                url = DOWNLOAD_URL.replace("{0}", acadYear);
-                path = Paths.get("data", "moduleInfo_{0}.json".replace("{0}", acadYear));
-                if (!Files.exists(path)) {
-                    try (InputStream in = new URL(url).openStream()) {
-                        Files.copy(in, path);
-                    }
+                fileName = "json/moduleInfo_{0}.json".replace("{0}", acadYear);
+                ClassLoader classLoader = new ModuleDataImporter().getClass().getClassLoader();
+                file = new File(classLoader.getResource(fileName).getFile());
+                if (!file.exists()) {
+                    throw new Exception("Resource " + fileName + " is not found.");
                 }
-                Optional<JsonSerializableModule[]> optionalModules = JsonUtil.readJsonFile(path,
+                Optional<JsonSerializableModule[]> optionalModules = JsonUtil.readJsonFile(Path.of(file.getPath()),
                     JsonSerializableModule[].class);
                 if (optionalModules.isPresent()) {
                     JsonSerializableModule[] moduleArray = optionalModules.get();
                     for (JsonSerializableModule m : moduleArray) {
                         try {
                             module = m.toModelType();
-                            if (modules.contains(module)) {
-                                int a = 1;
-                            } else {
+                            if (!modules.contains(module)) {
                                 modules.add(module);
                             }
                         } catch (IllegalValueException ex) {
@@ -56,7 +54,8 @@ public class ModuleDataImporter {
                     }
                 }
             } catch (Exception e) {
-                System.out.println(e);
+                System.err.println("Error loading file: " + fileName);
+                System.err.println(e);
             }
         }
         return modules;
